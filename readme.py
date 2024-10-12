094d497751d4d62cae8996b6c2831a93c6c88a63
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2024-10-12 21:11:36 +0000

    readme example with entry

--- readme.py
+++ readme.py
@@ -1,18 +1,21 @@
-from abacus import Book
+from abacus import Book, Entry
 
 book = Book.new()
 book.chart.assets.append("cash")
 book.chart.capital.append("equity")
+book.chart.liabilities.append("vat")
+book.chart.name("vat", "VAT payable")
 book.chart.income.append("sales")
 book.chart.offset("sales", "refunds")
 book.chart.expenses.append("salaries")
 book.post_double("Initial investment", debit="cash", credit="equity", amount=10000)
-book.post_double("Sold services", debit="cash", credit="sales", amount=6500)
+book.post(Entry("Sold services with VAT").dr("cash", 6000).cr("sales", 5000).cr("vat", 1000))
 book.post_double("Made refund", debit="refunds", credit="cash", amount=500)
-book.post_double("Paid salaries", debit="salaries", credit="cash", amount=1000)
+book.post_double("Paid salaries", debit="salaries", credit="cash", amount=1500)
 book.close()
 assert book.ledger.balances == {
-    "cash": 15000,
+    "cash": 14000,
     "equity": 10000,
-    "retained_earnings": 5000,
+    "vat": 1000,
+    "retained_earnings": 3000,
 }
