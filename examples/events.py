dd181cb3e6fd789c1b45515e1b6b65edfcbcf372
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2024-11-13 13:33:07 +0000

    events.py has `Drop` primitive

--- examples/events.py
+++ examples/events.py
@@ -5,20 +5,21 @@ The main class is `Ledger`. You can modify the state of ledger by applying opera
 The basic operations are:
 - `Add` and `Offset` to add regular and contra accounts,
 - `Debit` and `Credit` to change account balances,
-- `Drop` to deactivate empty account.
+- `PeriodEnd` to mark accounting period end,
+- `Drop` to deactivate an empty account.
 
 The compound operations are:
 - `Account` to specify an account together with contra accounts,
 - `Initial` to open ledger with initial balances,
 - `Double` to make a double entry transaction,
 - `Multiple` to make a multiple entry transaction,
-- `Transfer` to transfer account balance to another account,
-- `Close` to close ledger to earnings account at period end.
+- `Transfer` to move account balance to another account,
+- `Close` for closing accounts at period end.
 
 Each compound operation consists of a sequence of basic operations.
 
-`Ledger.history` attribute holds a sequence of events that where applied to the ledger.
-You can re-run the events on empty ledger and will get the same state of ledger.
+`Ledger.history` holds a sequence of events that where applied to the ledger.
+You can re-run the events on empty ledger and will arrive to the same state of ledger.
 """
 
 from abc import ABC, abstractmethod
@@ -27,7 +28,10 @@ from copy import deepcopy
 from dataclasses import dataclass, field
 from decimal import Decimal
 from enum import Enum
+from pathlib import Path
 from typing import ClassVar, Iterable, Iterator, Sequence
+
+Numeric = int | float | Decimal
 
 
 class AbacusError(Exception):
@@ -47,7 +51,7 @@ class T5(Enum):
 
 class Operation(object):
     """A unit of change of the ledger state.
-    
+
     Types of operations:
     - Account, Charting: change chart of accounts,
     - Posting: change account balances,
@@ -82,17 +86,19 @@ class Offset(Charting):
     parent: str
     name: str
 
+
 @dataclass
 class Drop(Charting):
     """Drop account if the account and its contra accounts have zero balances."""
 
     name: str
 
+
 @dataclass
 class Account(ABC, Operation):
     name: str
-    title: str | None = None
     contra_accounts: list[str] = field(default_factory=list)
+    title: str | None = None
     t: ClassVar[T5]
 
     def __iter__(self) -> Iterator[Add | Offset]:
@@ -123,15 +129,24 @@ class Expense(Account):
 
 @dataclass
 class Transfer(Closing):
-    """Transfer account balance to another account and delete account."""
+    """Transfer account balance to another account."""
 
     from_account: str
     to_account: str
 
 
 @dataclass
+class PeriodEnd(Closing):
+    """Mark end of accounting period.
+
+    At this point we shall save the state of ledger before closing it.
+    The saved ledger will be used for the income statement.
+    """
+
+
+@dataclass
 class Close(Closing):
-    """Close ledger to earnings account. Saves state of ledger before close."""
+    """Close ledger to earnings account."""
 
     earnings_account: str
 
@@ -151,6 +166,7 @@ class Credit(Posting):
     account: str
     amount: int | float | Decimal
 
+
 @dataclass
 class Double(Posting):
     """Double-entry transaction."""
@@ -166,10 +182,10 @@ class Double(Posting):
 
 @dataclass
 class Unbalanced(Posting):
-    """Multiple entry this is not balanced by debit and credit."""
+    """Multiple entry that may not be balanced by debits and credits."""
 
-    debits: list[tuple[str, int | float | Decimal]] = field(default_factory=list)
-    credits: list[tuple[str, int | float | Decimal]] = field(default_factory=list)
+    debits: list[tuple[str, Numeric]] = field(default_factory=list)
+    credits: list[tuple[str, Numeric]] = field(default_factory=list)
 
     def __iter__(self):
         for account, amount in self.debits:
@@ -197,7 +213,7 @@ class Unbalanced(Posting):
 
 
 class Multiple(Unbalanced):
-    """Multiple entry that is balanced by debit and credit."""
+    """Multiple entry that is balanced by debits and credits."""
 
     def __post_init__(self):
         self.validate()
@@ -213,15 +229,11 @@ class Multiple(Unbalanced):
         raise AbacusError(f"Debits {ds} and credits {cs} are not balanced for {self}.")
 
 
-Numeric = int | float | Decimal
-
-
 @dataclass
 class Initial(Posting):
     """Open ledger with initial balances."""
 
     balances: list[tuple[str, Numeric]]
-
 
 
 @dataclass
@@ -249,7 +261,6 @@ class CreditAccount(TAccount):
         self.balance -= Decimal(amount)
 
 
-
 @dataclass
 class Contra:
     """Contra account, refers to an existing regular account."""
@@ -257,9 +268,15 @@ class Contra:
     name: str
 
 
-def must_not_exist(chart, name):
-    if name in chart:
+def must_not_exist(collection, name):
+    if name in collection:
         raise AbacusError(f"Account {name} already exists.")
+
+
+def must_exist(collection, name):
+    if name not in collection:
+        raise AbacusError(f"Account {name} not found.")
+
 
 class ChartDict(UserDict[str, T5 | Contra]):
     def by_type(self, t: T5) -> list[str]:
@@ -272,49 +289,82 @@ class ChartDict(UserDict[str, T5 | Contra]):
             if parent == Contra(name)
         ]
 
-    def close_contra_accounts(self, t: T5) -> Iterable[Transfer]:
+    def close_contra_accounts(self, t: T5) -> Iterable[tuple[str, str]]:
         for account in self.by_type(t):
             for contra in self.find_contra_accounts(account):
-                yield Transfer(contra, account)
+                yield (contra, account)
 
-    def close_type(self, t: T5, earnings_account: str) -> Iterable[Transfer]:
+    def close_type(self, t: T5, earnings_account: str) -> Iterable[tuple[str, str]]:
         for account in self.by_type(t):
-            yield Transfer(account, earnings_account)
+            yield (account, earnings_account)
 
-    def close(self, earnings_account: str) -> Iterable[Transfer]:
+    def close(self, earnings_account: str) -> Iterable[tuple[str, str]]:
         for t in (T5.Income, T5.Expense):
             yield from self.close_contra_accounts(t)
             yield from self.close_type(t, earnings_account)
 
 
-Primitive = Add | Offset | Drop | Debit | Credit
+Primitive = Add | Offset | Debit | Credit | PeriodEnd | Drop
+
 
 @dataclass
 class Event:
+    action: Operation
     primitives: list[Primitive]
-    note: str | None = None
+    note: str | None
+
+
+@dataclass
+class History:
+    events: list[Event] = field(default_factory=list)
+
+    def __iter__(self):
+        yield from self.events
+
+    def append(
+        self, action: Operation, primitives: list[Primitive], note: str | None = None
+    ):
+        self.events.append(Event(action, primitives, note))
+
+    @property
+    def primitives(self) -> Iterable[Primitive]:
+        for event in self.events:
+            yield from event.primitives
+
+    @property
+    def actions(self) -> Iterable[Primitive]:
+        for event in self.events:
+            yield from event.primitives
+
+    def save(self, path: str | Path):
+        pass
+
+    @classmethod
+    def load(cls, path: str | Path):
+        pass
+
+    def to_ledger(self):
+        """Re-create ledger from history of actions."""
+        return Ledger.from_list(self.actions)
 
 
 @dataclass
 class Ledger:
     accounts: dict[str, TAccount] = field(default_factory=dict)
     chart: ChartDict = field(default_factory=ChartDict)
-    history: list[Event] = field(default_factory=list)
+    history: History = field(default_factory=History)
     accounts_before_close: dict[str, TAccount] | None = None
 
     def is_closed(self) -> bool:
         return self.accounts_before_close is not None
-    
+
     @classmethod
-    def from_accounts(cls, accounts: Sequence[Account]):
+    def from_accounts(cls, accounts: Iterable[Account]): # just shugar
         return cls.from_list(accounts)
 
     @classmethod
-    def from_list(cls, actions: Sequence[Operation]):
-        ledger = cls()
-        for action in actions:
-            ledger.apply(action)
-        return ledger
+    def from_list(cls, actions: Iterable[Operation]):
+        return cls().apply_many(actions)
 
     @property
     def balances(self):
@@ -339,14 +389,30 @@ class Ledger:
             return Double(to_account, from_account, balance)
         else:
             return Double(from_account, to_account, balance)
-        
-    def apply_account(self, account: Account) -> list[Primitive]:
-        operations = []
-        for action in iter(account):
-            operations.extend(self.apply_charting(action))
-        return operations    
 
-    def apply_charting(self, action: Charting) -> list[Primitive]:
+    def initial_entry(self, balances: list[tuple[str, Numeric]]) -> Multiple:
+        posting = Multiple()
+        for account, amount in balances:
+            if self.is_debit_account(account):
+                posting.debit(account, amount)
+            else:
+                posting.credit(account, amount)
+        if posting.is_balanced():
+            return posting
+        else:
+            raise AbacusError(f"Unbalanced {balances}")
+
+    def apply_from(self, action: Account | Multiple) -> list[Primitive]:
+        """Get primitives from account or multiple entry and use them to change ledger."""
+        primitives: list[Primitive] = []
+        for operation in iter(action):
+            primitives.extend(self._apply(operation))
+        return primitives
+
+    def _apply(self, action: Operation) -> list[Primitive]:
+        if isinstance(action, Account):
+            return self.apply_from(action)
+        operations: list[Primitive] = []
         match action:
             case Add(name, t):
                 must_not_exist(self.chart, name)
@@ -363,80 +429,56 @@ class Ledger:
             case Drop(name):
                 if not self.accounts[name].is_empty():
                     raise AbacusError(f"Account {name} is not empty.")
+                    # TODO: check contra accounts are empty too
                 del self.accounts[name]
                 return [action]
-            case _:
-                raise AbacusError(f"Unknown {action}")
-    
-    def initial_entry(self, balances: list[tuple[str, Numeric]]) -> Multiple:
-        posting = Multiple()
-        for account, amount in balances:
-            if self.is_debit_account(account):
-                posting.debit(account, amount)
-            else:
-                posting.credit(account, amount)
-        if posting.is_balanced():
-            return posting
-        else:
-            raise AbacusError(f"Unbalanced {balances}")
-
-    def apply_entry(self, entry: Posting) -> list[Primitive]:
-        match entry:
             case Debit(account, amount):
+                must_exist(self.accounts, account)
                 self.accounts[account].debit(amount)
-                return [entry]
+                return [action]
             case Credit(account, amount):
+                must_exist(self.accounts, account)
                 self.accounts[account].credit(amount)
-                return [entry]
-            case Double(debit, credit, amount):
-                self.accounts[debit].debit(amount)
-                self.accounts[credit].credit(amount)
-                return list(entry)
+                return [action]
+            case Double(_, _, _):
+                debit_entry, credit_entry = list(action)
+                self._apply(debit_entry)
+                self._apply(credit_entry)
+                return [debit_entry, credit_entry]
             case Initial(balances):
                 posting = self.initial_entry(balances)
-                return self.apply_entry(posting)
+                return self._apply(posting)
             case Multiple(_, _):
-                for primitive in entry:
-                    self.apply_entry(primitive)
-                return list(entry)
-            case _:
-                raise AbacusError(f"Unknown {entry}")
-
-    def apply_closing(self, action: Closing) -> list[Primitive]:
-        operations: list[Primitive] = []
-        match action:
+                return self.apply_from(action)
             case Transfer(from_account, to_account):
-                # transfer balance
                 transfer_entry = self.transfer_entry(from_account, to_account)
-                self.apply_entry(transfer_entry)
-                operations.extend(transfer_entry)
-                # drop account
-                drop = Drop(from_account)
-                self.apply_charting(drop)
-                operations.append(drop)
+                operations.extend(self._apply(transfer_entry))
                 return operations
-            case Close(earnings_account):
-                operations = []
+            case PeriodEnd():
                 self.accounts_before_close = deepcopy(self.accounts)
-                for transfer in self.chart.close(earnings_account):
-                    operations.extend(self.apply_closing(transfer))
+                return [action]
+            case Close(earnings_account):
+                for operation in self.close_ledger(earnings_account):
+                    operations.extend(self._apply(operation))
                 return operations
             case _:
                 raise AbacusError(f"Unknown {action}")
 
-    def apply(self, action: Operation, note: str | None=None) -> list[Primitive]:
-        if isinstance(action, Account):
-            operations = self.apply_account(action)
-        elif isinstance(action, Charting):
-            operations = self.apply_charting(action)
-        elif isinstance(action, Posting):
-            operations = self.apply_entry(action)
-        elif isinstance(action, Closing):
-            operations = self.apply_closing(action)
-        else:
-            raise AbacusError(f"Unknown {action}")
-        self.history.append(Event(operations, note))
-        return operations
+    def close_ledger(self, earnings_account: str) -> Iterable[Operation]:
+        yield PeriodEnd()
+        for from_account, to_account in self.chart.close(earnings_account):
+            yield Transfer(from_account, to_account)
+            yield Drop(from_account)
+
+    def apply(self, action: Operation, note: str | None = None):
+        operations = self._apply(action)
+        self.history.append(action, operations, note)
+        return self
+    
+    def apply_many(self, actions: Sequence[Operation]):
+        for action in actions:
+            self.apply(action, note=None)
+        return self
 
 
 # Create accounts
@@ -457,17 +499,16 @@ entries = [
     Double("refunds", "cash", 15),
     Double("voids", "cash", 15),
     Double("salaries", "cash", 50),
-    # Close accounts
+    # Close period end
     Close("retained_earnings"),
 ]
 
-ledger = Ledger.from_accounts(accounts)
-for e in entries:
-    ledger.apply(e)
-    print(e, "translates to", ledger.events[-1].primitives)
+ledger = Ledger.from_accounts(accounts).apply_many(entries)
+for event in ledger.history:    
+    print(event.action, "translates to:\n ", event.primitives)
 print(ledger.chart)
 print(ledger.balances)
-assert len(ledger.events) == len(accounts) + len(entries)
+assert len(ledger.history.events) == len(accounts) + len(entries)
 assert ledger.balances == {
     "cash": 50,
     "equity": 8,
@@ -484,7 +525,8 @@ assert ledger.chart == {
     "retained_earnings": T5.Equity,
     "vat": T5.Liability,
 }
-ledger2 = Ledger.from_list([p for event in ledger.events for p in event.primitives])
+ledger2 = ledger.history.to_ledger()
 assert ledger2.balances == ledger.balances
 if ledger.accounts_before_close is not None:
-   assert len(ledger.accounts_before_close) > 3
+    assert len(ledger.accounts_before_close) > 3
+assert ledger2.is_closed() is True  # should be true
