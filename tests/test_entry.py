dbe4fbe5f5b27f3f1062d5dbf5e36f56cea9d74a
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2024-11-17 11:06:09 +0000

    renamed tests

--- tests/test_entry.py
+++ tests/test_entry.py
@@ -30,3 +30,64 @@ def test_entry_amount():
 def test_entry_no_amount_raises_error():
     with pytest.raises(AbacusError):
         Entry("Entry with no amount").debit("cash")
+
+from decimal import Decimal
+
+from pytest import fixture
+
+from abacus import Credit, Debit, Double, Entry, Multiple
+
+
+def test_double():
+    d = Double("cash", "equity", 10)
+    assert list(d) == [
+        Debit(account="cash", amount=Decimal(10)),
+        Credit(account="equity", amount=Decimal(10)),
+    ]
+
+
+@fixture
+def reference_entry():
+    return [
+        Debit(account="cash", amount=Decimal(10)),
+        Credit(account="equity", amount=Decimal(8)),
+        Credit(account="retained_earnings", amount=Decimal(2)),
+    ]
+
+
+def test_multiple(reference_entry):
+    m = Multiple(
+        debits=[("cash", Decimal(10))],
+        credits=[("equity", Decimal(8)), ("retained_earnings", Decimal(2))],
+    )
+    assert list(m) == reference_entry
+
+
+def test_entry(reference_entry):
+    e = (
+        Entry("Some entry")
+        .debit("cash", 10)
+        .credit("equity", 8)
+        .credit("retained_earnings", 2)
+    )
+    assert list(e) == reference_entry
+
+
+@pytest.mark.entry
+def test_how_it_fails():
+    with pytest.raises(AbacusError):
+        Multiple.from_list([Debit("cash", 10)])
+
+
+@pytest.mark.entry
+def test_opening_fails(toy_dict):
+    with pytest.raises(AbacusError):
+        toy_dict.initial_entry(dict(cash=10, equity=8))
+
+
+@pytest.mark.entry
+def test_opening_entry(toy_dict):
+    opening_dict = dict(cash=10, equity=8, re=2)
+    entry = toy_dict.initial_entry(opening_dict)
+    assert list(entry) == [Debit("cash", 10), Credit("equity", 8), Credit("re", 2)]
+
--- tests/test_ledger.py
+++ tests/test_ledger.py
@@ -1,23 +1,15 @@
-import pytest  # type: ignore
-
-from abacus import (  # ,; BalanceSheet,; ChartDict,; CreditAccount,; DebitAccount,; Regular,; ReportDict,; is_balanced,
-    AbacusError,
-    Credit,
-    Debit,
+from abacus import (
     Double,
     Income,
     Ledger,
-    Multiple,
 )
 from abacus.ledger import BalanceSheet, CreditAccount, DebitAccount, ReportDict
 
 
-@pytest.mark.ledger
 def test_ledger_keys(toy_ledger):
     assert list(toy_ledger.accounts) == ["cash", "equity", "re"]
 
 
-@pytest.mark.ledger
 def test_ledger_open(toy_ledger):
     entry = toy_ledger.chart.initial_entry(dict(cash=2, equity=2))
     toy_ledger.apply(entry)
@@ -35,13 +27,11 @@ def test_balance_sheet_is_not_balanced():
     )
 
 
-@pytest.mark.report
 def test_net_earnings(toy_ledger):
     toy_ledger.apply_many([Income("sales"), Double("cash", "sales", 10)])
     assert toy_ledger.income_statement().net_earnings == 10
 
 
-@pytest.mark.report
 def test_balance_sheet(toy_dict):
     ledger = Ledger()
     ledger.chart = toy_dict
@@ -56,32 +46,14 @@ def test_balance_sheet(toy_dict):
     )
 
 
-@pytest.mark.report
 def test_balance_sheet_again(toy_ledger):
     assert toy_ledger.close("re").balance_sheet() == BalanceSheet(
         assets=dict(cash=10), equity=dict(equity=10, re=0), liabilities=dict()
     )
 
 
-@pytest.mark.report
 def test_balances(toy_ledger):
     assert toy_ledger.balances == dict(cash=10, equity=10, re=0)
 
 
-@pytest.mark.entry
-def test_opening_fails(toy_dict):
-    with pytest.raises(AbacusError):
-        toy_dict.initial_entry(dict(cash=10, equity=8))
 
-
-@pytest.mark.entry
-def test_opening_entry(toy_dict):
-    opening_dict = dict(cash=10, equity=8, re=2)
-    entry = toy_dict.initial_entry(opening_dict)
-    assert list(entry) == [Debit("cash", 10), Credit("equity", 8), Credit("re", 2)]
-
-
-@pytest.mark.entry
-def test_how_it_fails():
-    with pytest.raises(AbacusError):
-        Multiple.from_list([Debit("cash", 10)])
