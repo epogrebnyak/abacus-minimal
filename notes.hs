f4c47f61ae544ca5e4de45247c996db1480eddba
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2024-12-19 22:56:25 +0000

    toAccoutnMap

--- notes.hs
+++ notes.hs
@@ -6,15 +6,41 @@ type Name = String
 type Balances = Map.Map Name Amount
 data Side = Debit | Credit deriving Show
 data TAccount = TAccount Side Amount deriving Show
+
+emptyT :: Side -> TAccount
+emptyT side = TAccount side 0
+
 type AccountMap = Map.Map Name TAccount
 data T5 = Asset | Expense | Equity | Liability | Income deriving Show
-data Chart = Chart Name (Map.Map Name T5) (Map.Map Name Name) deriving Show
+
+
+-- Chart of accounts
+data Role = Regular T5 | Contra Name deriving Show
+type ChartM = Map.Map Name Role
+
+regular :: Name -> T5 -> ChartM -> ChartM
+regular name t = Map.insert name (Regular t)
+
+offset :: Name -> Name -> ChartM -> ChartM
+offset name contra = Map.insert contra (Contra name) 
+
+ask :: Name -> ChartM -> Maybe Side
+ask name cm = case Map.lookup name cm of
+    Just (Regular t) -> Just (which t)
+    Just (Contra name) -> revert <$> ask name cm
+    Nothing -> Nothing
+
+z = offset "re" "ts" (emptyM "re")
+
+data Chart = Chart Name ChartM deriving Show
 data Ledger = Ledger Chart AccountMap (Maybe AccountMap) deriving Show
 
 -- Create an empty Chart with given retained earnings account name
+emptyM :: Name -> ChartM
+emptyM re = regular re Equity Map.empty
+
 emptyChart :: Name -> Chart
-emptyChart re = Chart re regulars Map.empty
-    where regulars = Map.fromList [(re, Equity)] 
+emptyChart re = Chart re (emptyM re)
 
 which :: T5 -> Side
 which Asset = Debit
@@ -25,42 +51,27 @@ revert :: Side -> Side
 revert Debit = Credit
 revert Credit = Debit
 
-zero :: Amount
-zero = 0
-
--- Get side of a given account name 
--- getSide :: Chart -> Name -> Maybe Side
--- getSide (Chart _ regulars _) name = case Map.lookup name regulars of
---     Just t5 -> Just $ reverse $ which t5
---    Nothing -> Nothing 
-
--- TODO: Implement the following function
--- Get list of contra accounts with their sides
--- getListContra :: Chart -> [(Name, Side)]
--- getListContra (Chart _ regulars contras) =
---     [(name, revert $ which t5) | (name, contraName) <- Map.toList contras, Just t5 <- [Map.lookup contraName regulars]]
-
-
--- Convert Chart to AccountMap
-fromChart :: Chart -> AccountMap
-fromChart (Chart _ regulars contras) = Map.fromList $ xs ++ ys
+-- This needs a fix
+toAccountMap :: ChartM -> AccountMap
+toAccountMap cm = Map.fromList [(name, fromRole role) | (name, role) <- Map.toList cm]
   where
-    get = Map.toList
-    xs = [(name, TAccount (which t5) zero) | (name, t5) <- get regulars]
-    ys = []
-
-
+    fromRole role = case role of
+      Regular t -> Just (which t)
+      Contra contraName -> case ask contraName cm of
+        Just side -> Just (revert side)
+        Nothing -> Nothing
 
 -- Create an empty Ledger with the given retained earnings name
 emptyLedger :: Name -> Ledger
-emptyLedger re = let
-    chart = emptyChart re
-    in Ledger chart (fromChart chart) Nothing
+emptyLedger re = Ledger (Chart re e) (toAccountMap e) Nothing
+    where 
+        e = emptyM re
 
 -- Main function to demonstrate the creation of a Ledger
 main :: IO ()
 main = do
     let ledger = emptyLedger "re"
+    putStrLn (show (ask "ts" z))
     putStrLn "This is ledger:"
     putStrLn (show ledger)
 
