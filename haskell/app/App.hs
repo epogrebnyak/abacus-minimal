571928e6301399b8be233c5bc801e1457b9ee9c8
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2024-12-30 20:04:36 +0300

    renamed app to App.hs and  lib to Abacus.hs (cabal run passes)

--- haskell/app/App.hs
+++ haskell/app/App.hs
@@ -1,8 +1,8 @@
 module Main where
 
-import qualified MyLib (someFunc)
+import qualified Abacus (someFunc)
 
 main :: IO ()
 main = do
   putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  Abacus.someFunc
--- haskell/src/Abacus.hs
+++ haskell/src/Abacus.hs
@@ -1,4 +1,4 @@
-module MyLib (someFunc) where
+module Abacus (someFunc) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
